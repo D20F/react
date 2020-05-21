01e9ab32c064c40d581d0c92e1454dbfbcf57749
fb <2442563148@qq.com>
2020-05-21 17:50:19 +0800

    推送一下

---
+++ src/view/home/Home.js
@@ -0,0 +1,29 @@
+import React, { createContext } from 'react';
+import ReactDOM from 'react-dom';
+// import logo from '../../assets/img/logo.svg';
+import './Home.css';
+
+class MouseTracker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ height: '100vh' }} onMouseMove={this.handleMouseMove}>
+        <h1>移动鼠标!</h1>
+        <p>当前的鼠标位置是 ({this.state.x}, {this.state.y})</p>
+      </div>
+    );
+  }
+}
+export default MouseTracker;
